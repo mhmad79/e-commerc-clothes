73bbbac69f08b185c6105a2485c94df2c8dd079d
mohmeed <mhmadkhlifah79@gmail.com>
2025-05-11 15:52:54 +0300

    w

--- src/app/category/[categoryName]/page.tsx
+++ src/app/category/[categoryName]/page.tsx
@@ -1,5 +1,6 @@
-import ProductCard from '@/components/productsCart';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import ProductCard from '@/components/productsCart';
 
 type Product = {
   id: number;
@@ -10,40 +11,37 @@ type Product = {
   category: string;
 };
 
-export default async function CategoryPage({ params }: { params: { categoryName: string } }) {
-  try {
-    const res = await fetch(`https://fakestoreapi.com/products/category/${params.categoryName}`);
+type CategoryPageProps = {
+  params: {
+    categoryName: string;
+  };
+};
 
-    if (!res.ok) {
-      // إذا كان هناك خطأ في الاستعلام أو الفئة غير موجودة
-      notFound();
-      return; // لا حاجة للاستمرار في تنفيذ الكود بعد استخدام notFound()
-    }
-
-    const products: Product[] = await res.json();
-
-    if (products.length === 0) {
-      notFound(); // عرض صفحة 404 في حالة عدم وجود منتجات
-      return;
-    }
-
-    return (
-      <div>
-        <section className="products py-16 bg-gray-50">
-          <div className="container mx-auto text-center">
-            <h2 className="text-3xl font-bold mb-8">{params.categoryName} Products</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-              {products.map((product: Product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  } catch (error) {
-    // التعامل مع الأخطاء الأخرى
-    console.error('Error fetching category data:', error);
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const res = await fetch(`https://fakestoreapi.com/products/category/${params.categoryName}`);
+  
+  if (!res.ok) {
     notFound();
   }
+
+  const products: Product[] = await res.json();
+
+  if (!products.length) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <section className="products py-16 bg-gray-50">
+        <div className="container mx-auto text-center">
+          <h2 className="text-3xl font-bold mb-8">{params.categoryName} Products</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {products.map((product: Product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
