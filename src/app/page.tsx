bf75b8e949911fd6358462a93ca89a268c0a68a2
mohmeed <mhmadkhlifah79@gmail.com>
2025-09-07 15:52:56 +0300

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Carousel from "@/components/carousel";
-import Hero from "@/components/hero";
-import ProductCard from "@/components/productsCart";
+
 import Products from "./product/page";
 
 
